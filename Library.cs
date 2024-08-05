e67b18406fe7a3ae2bc2d72c9f8a4d193d889fe3
burak <burak@BURAK>
2024-08-05 21:24:12 +0300

    Proje dosyası ekle.

---
+++ Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaKutuphane
+{
+    public class Library
+    {
+
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+        public int PageNumb { get; set; }
+        public string Publisher { get; set; }
+        public DateTime DateOfRegister { get; set; }
+
+        static private int BookCount;
+        static public int BookCount_
+        {
+            get
+            {
+                return BookCount;
+            }
+            set
+            {
+                BookCount = value;
+            }
+            
+        }
+        static Library()
+        {
+            BookCount = 0;
+        }
+        static public void DisplayBookNumber()
+        {
+            Console.WriteLine("Total Book's Number: " + BookCount);
+        }
+
+
+        public Library()
+        {
+            Console.WriteLine("Date of Registration : " + DateTime.Now);
+        }
+        public Library(string bookname, string authorname, string authorsurname, int pagenumber, string publisher) :this()
+
+        {
+            bookname = Name;
+            authorname = AuthorName;
+            authorsurname = AuthorSurname;
+            pagenumber = PageNumb;
+            publisher = Publisher;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using PatikaKutuphane;
+
+Library book1 = new Library()
+{
+    Name = "Bir Kitap",
+    AuthorName = "Ahmet",
+    AuthorSurname = "Yılmaz",
+    PageNumb = 320,
+    Publisher = "Alfa Yayınları"
+};
+
+Console.WriteLine("Book Name: " + book1.Name + "\nAuthor Name: " + book1.AuthorName + "\nAuthor Surname: " + book1.AuthorSurname + "\nPage Numbers: " + book1.PageNumb + "\nPublisher: " + book1.Publisher);
+Library.BookCount_++;
+Library book2 = new("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayıncılık");
+Console.WriteLine("Book Name: " + book2.Name + "\nAuthor Name: " + book2.AuthorName + "\nAuthor Surname: " + book2.AuthorSurname + "\nPage Numbers: " + book2.PageNumb + "\nPublisher: " + book2.Publisher);
+Library.BookCount_++;
+Library.DisplayBookNumber();
+
+
